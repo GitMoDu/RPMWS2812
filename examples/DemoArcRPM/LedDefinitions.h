82f19825e659752f73fca1c5bc79c3f37067d88a
André Pereira <deltazero@outlook.com>
2017-08-11 21:37:07 +0100

    Moved pin definition to proper header file.

--- examples/DemoArcRPM/LedDefinitions.h
+++ examples/DemoArcRPM/LedDefinitions.h
@@ -3,7 +3,6 @@
 #ifndef _LEDDEFINITIONS_h
 #define _LEDDEFINITIONS_h
 
-#define LED_DATA_PIN 2
 #define LED_COUNT 15
 
 #endif
--- examples/DemoArcRPM/PinDefinitions.h
+++ examples/DemoArcRPM/PinDefinitions.h
@@ -6,5 +6,6 @@
 #define SERIAL_TX_PIN 0
 #define SERIAL_RX_PIN 1
 
+#define LED_DATA_PIN 7
 #endif
 
